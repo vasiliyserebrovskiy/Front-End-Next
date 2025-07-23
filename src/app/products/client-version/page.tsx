8c5dc6336c8b05694db2900e8c00196b67708dc3
SerebrovskiyV <vasiliy.serebrovskiy@gmail.com>
2025-07-23 20:33:30 +0200

    done

--- src/app/products/client-version/page.tsx
+++ src/app/products/client-version/page.tsx
@@ -42,7 +42,13 @@ const ProductsClientVersion = () => {
               <li key={"product" + p.id} className={style.productCard}>
                 <h3>{p.title}</h3>
                 {/* <img src={p.images[0]} alt="product" /> */}
-                <Image src={p.images[0]} alt="Photo" width={150} height={200} />
+                <Image
+                  src={p.images[0]}
+                  alt="Photo"
+                  width={150}
+                  height={200}
+                  className="w-40 h-auto"
+                />
                 <span>{p.price} $</span>
                 {/* <p>{p.description}</p> */}
               </li>

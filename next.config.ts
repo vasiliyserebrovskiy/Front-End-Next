cf552937a038e9494f0dc37f9c1e678f3339d239
SerebrovskiyV <vasiliy.serebrovskiy@gmail.com>
2025-07-23 18:49:03 +0200

    all done

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,7 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   images: {
-    domains: ["i.imgur.com"],
+    unoptimized: true, // disables all optimization and domain checks
   },
 };
 
--- src/app/products/server-version/page.tsx
+++ src/app/products/server-version/page.tsx
@@ -1,5 +1,34 @@
-export const ProductsServerVersion = () => {
-  return <div>page ProductsServerVersion</div>;
+import { Product } from "@/types";
+import style from "../products.module.css";
+import Image from "next/image";
+
+export const ProductsServerVersion = async () => {
+  const res = await fetch("https://api.escuelajs.co/api/v1/products");
+
+  if (!res.ok) {
+    throw new Error(`Products fetch error: ${res.status} ${res.statusText}`);
+  }
+
+  const products: Product[] = await res.json();
+
+  return (
+    <div className={style.mainDiv}>
+      <h2>Products list</h2>
+      <div className={style.productsDiv}>
+        <ul>
+          {products.map((p) => (
+            <li key={"product" + p.id} className={style.productCard}>
+              <h3>{p.title}</h3>
+              {/* <img src={p.images[0]} alt="product" /> */}
+              <Image src={p.images[0]} alt="Photo" width={150} height={200} />
+              <span>{p.price} $</span>
+              {/* <p>{p.description}</p> */}
+            </li>
+          ))}
+        </ul>
+      </div>
+    </div>
+  );
 };
 
 export default ProductsServerVersion;

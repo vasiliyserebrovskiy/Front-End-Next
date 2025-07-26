92d0873eabd797e9d05ba8fb6e6e702d4e82a459
SerebrovskiyV <vasiliy.serebrovskiy@gmail.com>
2025-07-26 19:07:15 +0200

    styles was added to the creation product and category forms

--- src/app/categories/create/page.tsx
+++ src/app/categories/create/page.tsx
@@ -3,11 +3,26 @@ import React from "react";
 
 export default function CreateCategory() {
   return (
-    <div>
-      <form action={createCategory}>
-        <input type="text" name="name" placeholder="name" />
-        <input type="text" name="image" placeholder="image" />
-        <button type="submit">Submit</button>
+    <div className="flex justify-center gap-2 p-2">
+      <form
+        action={createCategory}
+        className="flex flex-col items-center border p-4 gap-4 rounded-2xl  border-green-600"
+      >
+        <input
+          type="text"
+          name="name"
+          placeholder="name"
+          className="border rounded-2xl p-2"
+        />
+        <input
+          type="text"
+          name="image"
+          placeholder="image"
+          className="border rounded-2xl p-2"
+        />
+        <button type="submit" className="border rounded-2xl p-2 border-blue-400 text-blue-700 hover:text-blue-300 cursor-pointer">
+          Submit
+        </button>
       </form>
     </div>
   );
--- src/app/products/create/page.tsx
+++ src/app/products/create/page.tsx
@@ -4,18 +4,48 @@ import React from "react";
 export default function CreateProduct() {
   return (
     <section className="flex justify-center p-3">
-      <div className="flex flex-col gap-2">
+      <div className="flex flex-col justify-center items-center gap-2 p-2">
         <h2>Create Product</h2>
         <form
           action={createProduct}
-          className="flex flex-col items-center gap-5"
+          className="flex flex-col items-center border p-4 gap-4 rounded-2xl  border-green-600"
         >
-          <input type="text" name="title" placeholder="title" />
-          <input type="text" name="price" placeholder="price" />
-          <input type="text" name="description" placeholder="description" />
-          <input type="text" name="categoryId" placeholder="categoryId" />
-          <input type="text" name="image" placeholder="image" />
-          <button type="submit">Submit</button>
+          <input
+            type="text"
+            name="title"
+            placeholder="title"
+            className="border rounded-2xl p-2"
+          />
+          <input
+            type="text"
+            name="price"
+            placeholder="price"
+            className="border rounded-2xl p-2"
+          />
+          <input
+            type="text"
+            name="description"
+            placeholder="description"
+            className="border rounded-2xl p-2"
+          />
+          <input
+            type="text"
+            name="categoryId"
+            placeholder="categoryId"
+            className="border rounded-2xl p-2"
+          />
+          <input
+            type="text"
+            name="image"
+            placeholder="image"
+            className="border rounded-2xl p-2"
+          />
+          <button
+            type="submit"
+            className="border rounded-2xl p-2 border-blue-400 text-blue-700 hover:text-blue-300 cursor-pointer"
+          >
+            Submit
+          </button>
         </form>
       </div>
     </section>
